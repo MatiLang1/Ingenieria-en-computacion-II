17810da422a71513b21e5831128776508bc7f444
Matias Lang <matiaslang124@gmail.com>
2026-01-30 17:42:17 -0300

    Mejoramos logica de control y agregamos comentarios

--- Entorno_real/ESP32.c
+++ Entorno_real/ESP32.c
@@ -23,6 +23,11 @@ unsigned long tiempoInicioBuzzer = 0;
 bool buzzerState = false;
 const unsigned long DURACION_BUZZER = 15000; //15 segundos
 
+//variables globales para manejo del parpadeo del LED rojo en el loop
+unsigned long ultimo_parpadeo = 0;
+bool ledState = LOW;
+const unsigned long intervalo_parapadeo = 500; // 500 ms parpadeo
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(DHTPIN, DHTTYPE);
@@ -30,8 +35,8 @@ DHT dht(DHTPIN, DHTTYPE);
 
 String estadoSistema = "NORMAL";
 
-unsigned long lastPublish = 0;
-const unsigned long interval = 60000; // 1 minuto
+unsigned long ultima_publicacion = 0;
+const unsigned long intervalo_publicacion = 60000; // 1 minuto
 
 void setupWiFi() {
   WiFi.begin(ssid, password);
@@ -59,36 +64,49 @@ void reconnectMQTT() {
   }
 }
 
-void evaluarEstado(float temp, int nivel) {
-  // Verificamos si hay condiciones de ALERTA
-  if (temp > 35 || nivel < 500) {
+// Definimos los valores umbrales para los sensores (POSIBLEMENTE HAY QUE ADAPTARLOS A LA ESP32, ESTOS SON LOS VALORES DE ARDUINO, CONSIDERA 700/1024 = 68% POR LO QUE EN ESP32 QUE USA 4096 SERIA 2848 SU 68%, REVISAR SI HAY QUE ESCALAR ESTOS VALORES SOLO DE LUZ Y DE NIVEL EL DE TEMPERATURA NO SE MODIFICA)
+#define TEMP_AVISO 28 // Grados °C
+#define TEMP_ALERTA 35
+#define LUZ_AVISO 700
+#define LUZ_ALERTA 900
+#define NIVEL_AVISO 300
+#define NIVEL_ALERTA 100
+
+void evaluarEstado(float temp, int luz, int nivel) {
+  bool alerta = (temp > TEMP_ALERTA) || (luz > LUZ_ALERTA) || (nivel < NIVEL_ALERTA);
+  bool aviso = (temp > TEMP_AVISO) || (luz > LUZ_AVISO) || (nivel < NIVEL_AVISO);
+
+  if (alerta) {
     estadoSistema = "ALERTA";
-    digitalWrite(LED_ROJO, HIGH);
-    digitalWrite(LED_VERDE, LOW);
-    
     // Si entra en alerta (aunque sea un segundo), activamos el buzzer
     buzzerState = true;
-    tiempoInicioBuzzer = millis(); // RECARGA: Siempre actualiza el tiempo inicial
-    tone(BUZZER, 1000); 
-    
-    Serial.println("¡ALERTA detectada! Buzzer encendido/recargado por 15s");
-  } else {
-    // Si sale de alerta, el sistema visual cambia, pero NO apagamos el buzzer acá
-    estadoSistema = "NORMAL";
-    digitalWrite(LED_ROJO, LOW);
-    digitalWrite(LED_VERDE, HIGH);
+    tiempoInicioBuzzer = millis(); // siempre actualiza el tiempo inicial (si pasaron 5 s quedan 10 s de buzzer, lo recarga a 15 s)
+    tone(BUZZER, 1000); // activamos el tono del buzzer
+    Serial.println("ALERTA detectada!");
+  } 
+  else if (aviso) {
+     estadoSistema = "AVISO";
+    Serial.println("Estado del sistema: AVISO");
+  }
+  else {
+    estadoSistema = "OK";
+    Serial.println("Estado del sistema: OK");
   }
 }
 
 void publishAll(float temp, int luz, int nivel) {
-  char buf[80];
+  char buf[80]; //declaramos un buffer (un espacio de memoria temporal de type char un string con capacidad para 80 caracteres). Es un array de caracteres donde almacenamos el string completo que enviaremos por mqtt
 
+  //sprintf nos permite formatear un string (como el printf pero en lugar de imprimir en consola, lo GUARDA en una variable, en este caso en buf). Asi formateamos el string completo que vamos a mandar por mqtt. Las / invertidas es para que el lenguaje entienda que las comillas de value son partes del string, el %.2f es reemplazado por el valor de la variable que le pasas (en este caso temp) el 2 indica 2 decimales la f es floar, en luz y nivel le pasas la d porque es int y en estado sistema le pasas la s pq es un string
   sprintf(buf, "{ \"value\": %.2f }", temp);
   client.publish("semillero/sensores/temperatura", buf);
 
+
+  //El método .publish() de la librería PubSubClient recibe dos parámetros: El topic (string). y el payload que tiene el contenido que se envia, dicho payload debe ser un array de caracteres
   sprintf(buf, "{ \"value\": %d }", luz);
   client.publish("semillero/sensores/luz", buf);
 
+  //sprintf sobreescribe buf cada vez que se usa para los distintos topics
   sprintf(buf, "{ \"value\": %d }", nivel);
   client.publish("semillero/sensores/nivel", buf);
 
@@ -115,23 +133,6 @@ void loop() {
   if (!client.connected()) reconnectMQTT();
   client.loop();
 
-  if (millis() - lastPublish >= interval) {
-    lastPublish = millis();
-
-    float temp = dht.readTemperature();
-    int luz = analogRead(LDR_PIN);
-    int nivel = analogRead(NIVEL_PIN);
-
-    evaluarEstado(temp, nivel);
-    publishAll(temp, luz, nivel);
-  }
-}
-
-
-void loop() {
-  if (!client.connected()) reconnectMQTT();
-  client.loop();
-
   //logica de apagado del buzzer
   if (buzzerState) {
     // Si ya pasaron 15s desde la ÚLTIMA recarga
@@ -139,18 +140,38 @@ void loop() {
       noTone(BUZZER);
       buzzerState = false;
       client.publish("semillero/estado/buzzer", "{ \"buzzer\": \"OFF\" }");
-      Serial.println("Buzzer apagado");
+      Serial.println("Buzzer apagado por Tiempo");
     }
   }
 
-  //Publicacion periodica
-  if (millis() - lastPublish >= interval) {
-    lastPublish = millis();
+  // Logica de los LEDs
+  digitalWrite(LED_VERDE, HIGH); // Led Verde siempre encendido
+
+  // Led Rojo depende del estado del sistema
+  if (estadoSistema == "ALERTA") {
+    digitalWrite(LED_ROJO, HIGH); // Rojo prendido fijo
+  } 
+  else if (estadoSistema == "AVISO") {
+    // Rojo parpadeando cada 500ms
+    if (millis() - ultimo_parpadeo >= intervalo_parapadeo) {
+      ultimo_parpadeo = millis();
+      ledState = !ledState;
+      digitalWrite(LED_ROJO, ledState);
+    }
+  } 
+  else {
+    // OK
+    digitalWrite(LED_ROJO, LOW); // Rojo apagado
+  }
+
+  //Publicacion periodica cada 1 minuto (60000 ms)
+  if (millis() - ultima_publicacion >= intervalo_publicacion) {
+    ultima_publicacion = millis();
     float temp = dht.readTemperature();
     int luz = analogRead(LDR_PIN);
     int nivel = analogRead(NIVEL_PIN);
 
-    evaluarEstado(temp, nivel);
+    evaluarEstado(temp, luz, nivel);
     publishAll(temp, luz, nivel);
   }
 }
